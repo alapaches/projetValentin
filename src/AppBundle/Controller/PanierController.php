ff80f66ee4c96b869232d39c30e073496da4035b
Julien SCULTEUR <juliensculteur@gmail.com>
2021-03-28 17:29:22 +0200

    fix: Ajout d'un service pour alleger le controller

--- src/AppBundle/Controller/PanierController.php
+++ src/AppBundle/Controller/PanierController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Produit;
+use AppBundle\Service\Panier\PanierService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -18,48 +19,22 @@ class PanierController extends Controller
     /**
      * @Route("/", name="panier_index")
      */
-    public function indexAction(SessionInterface $session) {
-        $longueur = count($session->get('panier', []));
-        
+    public function indexAction(PanierService $panierService) {
+        $longueur = count($panierService->getPanier());        
         $this->get('twig')->addGlobal('panierLongueur', $longueur);
-        $panier = $session->get('panier', []);
-        $produitRepository = $this->getDoctrine()->getManager()->getRepository(Produit::class);
-        $donneesPanier = [];
-
-        foreach($panier as $id => $quantite) {
-            $donneesPanier[] = [
-                'article' => $produitRepository->find($id),
-                'quantite' => $quantite
-            ];
-        }
-
-        $total = 0;
-
-        foreach($donneesPanier as $article) {
-            $totalArticle = $article['article']->getPrix() * $article['quantite'];
-            $total += $totalArticle;
-        }
 
         return $this->render('@App\Panier\index.html.twig', array(
-            'produits' => $donneesPanier,
-            'totalPanier' => $total
+            'produits' => $panierService->getDonneesPanier(),
+            'totalPanier' => $panierService->getTotal()
         ));
     }
 
     /**
      * @Route("/add/{id}", name="panier_add")
      */
-    public function addAction($id, SessionInterface $session) {
+    public function addAction($id, PanierService $panierService) {
         
-        $panier = $session->get('panier', []);
-
-        if(!empty($panier[$id])) {
-            $panier[$id] ++;
-        } else {
-            $panier[$id] = 1;
-        }
-
-        $session->set('panier', $panier);
+        $panierService->add($id);
 
         return $this->redirectToRoute("panier_index");
     }
@@ -67,14 +42,8 @@ class PanierController extends Controller
     /**
      * @Route("/delete/{id}", name="panier_delete")
      */
-    public function deleteAction($id, SessionInterface $session) {
-        $panier = $session->get('panier', []);
-
-        if(!empty($panier[$id])) {
-            unset($panier[$id]);
-        }
-
-        $session->set('panier', $panier);
+    public function deleteAction($id, PanierService $panierService) {
+        $panierService->delete($id);
 
         return $this->redirectToRoute("panier_index");
     }
---
+++ src/AppBundle/Service/Panier/PanierService.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\Service\Panier;
+
+use AppBundle\Repository\ProduitRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class PanierService {
+
+    protected $session;
+
+    protected $produitsRepository;
+
+    public function __construct(SessionInterface $session, ProduitRepository $produitsRepository) {
+        $this->session = $session;
+        $this->produitsRepository = $produitsRepository;
+    }
+
+    public function getPanier(): array {
+        $panier = $this->session->get('panier', []);
+
+        return $panier;
+    }
+
+    public function add($id) {
+        $panier = $this->session->get('panier', []);
+
+        if(!empty($panier[$id])) {
+            $panier[$id] ++;
+        } else {
+            $panier[$id] = 1;
+        }
+
+        $this->session->set('panier', $panier);
+    }
+
+    public function delete($id) {
+        $panier = $this->session->get('panier', []);
+
+        if(!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+
+        $this->session->set('panier', $panier);
+    }
+
+    public function getDonneesPanier() : array {
+
+        $panier = $this->session->get('panier', []);
+        $donneesPanier = [];
+
+        foreach($panier as $id => $quantite) {
+            $donneesPanier[] = [
+                'article' => $this->produitsRepository->find($id),
+                'quantite' => $quantite
+            ];
+        }
+
+        return $donneesPanier;
+        
+    }
+
+    public function getTotal() : float {
+
+        $total = 0;
+        $donnees = $this->getDonneesPanier();
+        foreach($donnees as $article) {
+            $totalArticle = $article['article']->getPrix() * $article['quantite'];
+            $total += $totalArticle;
+        }
+
+        return $total;
+    }
+}
